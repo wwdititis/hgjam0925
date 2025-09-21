8d8a5c6f79d99a8a036254814f1fb902d35e1537
Des <andressaptessari@gmail.com>
2025-09-20 22:34:02 -0300

    jp lesson added to main scene

--- minigames/jp_lesson/fallingBlocks.gd
+++ minigames/jp_lesson/fallingBlocks.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
 func _on_body_entered(body: Node):
+	print("Collided with:", body.name, " Groups:", body.get_groups())
 	if body.is_in_group("floor"):
+		print("FLOOR HIT -> freeing block")
 		queue_free()
 		Signals.emit_signal("block_free")
